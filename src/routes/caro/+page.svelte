efe28e01df6510ecdf2287bf53f2ca9afcd274c4
patttterson <49377371+patttterson@users.noreply.github.com>
2026-03-17 14:18:53 -0700

    center caro vertically

--- src/routes/caro/+page.svelte
+++ src/routes/caro/+page.svelte
@@ -5,9 +5,9 @@
 <style>
   .content {
     display: flex;
-    flex-direction: column;
     align-items: center;
     width: min(1000px, 75%);
     margin: 0 auto;
+    height: 100vh;
   }
 </style>
